4655b7743a39f1a853f178798b13b7822cce280d
Varun.Goel <Varun.Goel@GEMGN-220446>
2023-01-09 21:14:22 +0530

    Add project files.

---
+++ Assignment/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Login_Btt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Server=GEM-DEV-MISDB\SQLEXPRESS;Database=training_db ,Table = VR1_LR;Integrated Security = True");
+                con.Open();
+                string add_data = "SELECT * FROM [dbo].[user] where Email = @Email and Password = @PASSWORD";
+                SqlCommand cmd = new SqlCommand(add_data, con);
+                cmd.Parameters.AddWithValue("@Email", Email.Text);
+                cmd.Parameters.AddWithValue("@Password", Password.Text);
+                cmd.ExecuteNonQuery();
+                int Count = Convert.ToInt32(cmd.ExecuteScalar()); 
+                con.Close();
+                Email.Text = "";
+                Password.Text = "";
+                if(Count> 0)
+                {
+                    dosomething d1 = new dosomething();
+                    this.Close();
+                    d1.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Password or username is not correct");
+                }
+                
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Register r1 = new Register();
+            this.Close();
+            r1.Show();
+        }
+    }
+}
---
+++ Assignment/Register.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Assignment
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Bttn_Click(object sender , RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Server=GEM-DEV-MISDB\SQLEXPRESS;Database=training_db ,Table = VR1_LR;Integrated Security = True");
+                con.Open();
+                string add_data = "Insert into [dbo].[user] VALUES(@Email,Email.Text)";
+                SqlCommand cmd = new SqlCommand(add_data, con);
+                cmd.Parameters.AddWithValue("@Email", Email.Text);
+                cmd.Parameters.AddWithValue("@Password", Password.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Email.Text = "";
+                Password.Text = "";
+                MainWindow w1 = new MainWindow();
+                this.Close();
+                w1.Show();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
